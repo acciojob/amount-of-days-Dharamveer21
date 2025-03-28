e2c35bf313588372617641aa662c13452f560587
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2025-03-28 14:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+function checkLeapYear(year) {
+    let isLeapYear = ((year % 4) === 0);
+    let isCenturyYear = ((year % 100) === 0);
+
+    if (isCenturyYear) {
+        isLeapYear = ((year % 400) === 0);
+    }
+
+    return isLeapYear;
+}
+
+function daysOfAYear(year) {
+    let isLeapYear = checkLeapYear(year);
+    let numberOfDaysInYear = 365;
+
+    if (isLeapYear) {
+        numberOfDaysInYear = 366;
+    }
+
+    return numberOfDaysInYear;
+}
